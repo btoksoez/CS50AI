941adcd30cb6e1c83a365eebaed55b26e574e94f
Benian Toksöz <benian@Benians-Air.Home>
2024-02-08 13:54:17 +0000

    wrote most functions

--- Lecture_0/tictactoe/test.py
+++ Lecture_0/tictactoe/test.py
@@ -1,8 +1,28 @@
-from tictactoe import player
+from tictactoe import player, actions, result, winner, terminal, utility, max_value, min_value, minimax
 
 EMPTY = None
 x = "X"
+o = "O"
 
-print(player([[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, o, x ]]))
+board = [[x, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]]
+
+print(f"Player: {player(board)}\n")
+
+print(f"Actions: {actions(board)}\n")
+
+#print(f"Result: {result(board, (0, 1))}\n")
+
+print(f"Winner: {winner(board)}\n")
+
+#print(f"Utility: {utility(board)}\n")
+
+print(f"Terminal: {terminal(board)}\n")
+
+print(f"Max-Val: {max_value(board)}\n")
+print(f"Min-Val: {min_value(board)}\n")
+
+print(f"Best action: {minimax(board)}\n")
+
+
--- Lecture_0/tictactoe/tictactoe.py
+++ Lecture_0/tictactoe/tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 
-import math
+import math, copy
 
 X = "X"
 O = "O"
@@ -18,58 +18,175 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY]]
 
 
+def is_valid_board(board):
+    xs = sum(cell == X for row in board for cell in row)
+    os = sum(cell == O for row in board for cell in row)
+    if abs(xs - os) > 1:
+        return 0
+    return 1
+
+
+
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
 
-    #Initial game state                
+    #Initial game state
     initial = all(cell == EMPTY for row in board for cell in row)
-    turn = 
-    
-    if initial: 
+
+    #Check if valid board
+    if not is_valid_board(board):
+        raise Exception("Not valid board")
+
+    #Count empty fields
+    count_empty = sum(cell == EMPTY for row in board for cell in row)
+
+    if initial:
+        return X
+    if count_empty % 2 == 0:
+        return O
+    else:
         return X
 
-        
+
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    #Check if valid board
+    if not is_valid_board(board):
+        raise Exception("Not valid board")
+
+    available_actions = set()
+
+    #Add empty fields to available actions
+    for row, rows in enumerate(board):
+        for column, cell in enumerate(rows):
+            if cell == EMPTY:
+                available_actions.add((row, column))
+    return available_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    #Check if valid board
+    if not is_valid_board(board):
+        raise Exception("Not valid board")
+
+    #Check if valid action
+    if action not in actions(board):
+        raise Exception("Invalid Action")
+
+    #Make copy
+    res_board = copy.deepcopy(board)
+
+    #Unpacking row and column from action tuple
+    i, j = action
+
+    #Write new values into board
+    if player(board) == X:
+        res_board[i][j] = X
+    else:
+        res_board[i][j] = O
+    return res_board
+
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    #Check if valid board
+    if not is_valid_board(board):
+        raise Exception("Not valid board")
+
+    #Check if any player has won
+    for player in [X, O]:
+        #Horizontal
+        if any(all(cell == player for cell in row) for row in board):
+            return 1 if player == X else 0
+        #Vertical
+        if any(all(board[i][j] == player for i in range(3)) for j in range(3)):
+            return 1 if player == X else 0
+        #Diagonal
+        if all(board[i][i] == player for i in range(3)) or all(board[j][2-j] == player for j in range(3)):
+            return 1 if player == X else 0
+    return None
+
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board):
+        return True
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if not terminal(board):
+        raise Exception("Not a terminal state")
+    if winner(board) == 1:
+        return 1
+    elif winner(board) == 0:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    #best action for Player X
+    if player(board) == X:
+        v = -1
+        for action in actions(board):
+            if max_value(result(board, action)) == 1 and terminal(result(board, action)):
+                return (action)
+            if v <= max_value(result(board, action)):
+                v = max_value(result(board, action))
+                best_action = action
+
+    #Best action for Player O
+    if player(board) == O:
+        v = 1
+        for action in actions(board):
+            if min_value(result(board, action)) == -1 and terminal(result(board, action)):
+                return (action)
+            if v >= min_value(result(board, action)):
+                v = min_value(result(board, action))
+                best_action = action
+
+    return best_action
+
+
+def max_value(board):
+    if terminal(board):
+        return(utility(board))
+    v = float('-inf')
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+
+def min_value(board):
+    if terminal(board):
+        return(utility(board))
+    v = float('inf')
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
+
+
